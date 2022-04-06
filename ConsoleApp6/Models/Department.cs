b70d88a19876ca24204836829a62fab9b9af1d34
Nesrullah Hacizade <nesrullah.hacizade@gmail.com>
2022-04-06 10:00:12 +0400

    created task

---
+++ ConsoleApp6/Models/Department.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp6.Models
+{
+    internal class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        private List<Employee> employees;
+        public void AddEmployee(Employee employee)
+        {
+
+        }
+        public void GetEmployeeById(int id)
+        {
+
+        }
+        public void RemoveEmployee(int id)
+        {
+
+        }
+    }
+}
---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            if (!Directory.Exists(@"C:\Users\User\Desktop\Files"))
+            {
+                Directory.CreateDirectory(@"C:\Users\User\Desktop\Files");
+            }
+            if (!File.Exists(@"C:\Users\User\Desktop\Files\Database.json"))
+            {
+                File.Create(@"C:\Users\User\Desktop\Files\Database.json");
+            }
+
+            //bool check = true;
+            
+                Console.WriteLine("===== MENU =====");
+                Console.WriteLine("1.Add employee");
+                Console.WriteLine("2.Get employee by id");
+                Console.WriteLine("3.Remove employee");
+                Console.WriteLine("0.Quit");
+
+            
+
+        }
+    }
+}
